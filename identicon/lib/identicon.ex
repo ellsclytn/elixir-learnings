705e1380ea4221c417d8e36846e038fc5dde8899
Ellis Clayton <ellis@ellisclayton.info>
2016-11-27 17:42:26 +1000

    Section 8, Lesson 46
    - Add row mirroring
    - Filter blank cells from image grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,11 +4,29 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
+    grid = hex
     |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+    |> List.flatten
+    |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
